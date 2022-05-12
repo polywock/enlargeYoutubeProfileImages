3c2a8307c2960df404d1dae0e73084d2d0564dec
anon <noreply>
2022-05-12 06:39:01 -0400

    fix UI bugs

--- static/main.js
+++ static/main.js
@@ -18,14 +18,14 @@
         const {size, square} = JSON.parse(e.detail)
         const desc = Object.getOwnPropertyDescriptor(HTMLImageElement.prototype, "src")
         Object.defineProperty(HTMLImageElement.prototype, "src", {configurable: true, enumerable: true, get: desc.get, set: function(value) {
-            if (value.includes("https://yt3.ggpht.com") && this.parentElement.classList.contains("ytd-comment-renderer")) {
+            if (value.includes("https://yt3.ggpht.com") && this.parentElement.classList.contains("ytd-comment-renderer") && !document.querySelector("ytd-comments.ytd-multi-page-menu-renderer")?.contains(this.parentElement)) {
                 return desc.set.call(this, value.replace(/\=s\d+/, `=s${size}`))
             }
             return desc.set.call(this, value)
         }})
         
         const s = document.createElement("style")
-        s.innerHTML = `yt-img-shadow.ytd-comment-renderer {height: ${size}px !important; width: ${size}px !important; border-radius: ${square ? "0px" : "50%"} !important;} ytd-comment-replies-renderer {margin-left: ${size + 6}px !important;}` 
+        s.innerHTML = `#comments yt-img-shadow.ytd-comment-renderer {height: ${size}px !important; width: ${size}px !important; border-radius: ${square ? "0px" : "50%"} !important;} ytd-comment-replies-renderer {margin-left: ${size + 6}px !important;}` 
         document.documentElement.appendChild(s)
     }, {once: true, capture: true})
 
