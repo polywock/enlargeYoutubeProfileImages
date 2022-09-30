21de9d517b01817544bb1600bf7b528d2d5b0402
anon <noreply>
2022-09-30 05:32:36 -0400

    improved hover zoom

--- static/isolated.js
+++ static/isolated.js
@@ -18,39 +18,79 @@ const s = document.createElement("script")
 s.src = chrome.runtime.getURL("main.js")
 document.documentElement.appendChild(s)
 
-let isActive; 
+let activeRect; 
 let wrapper; 
-let wrapperImg; 
 
-function show(src) {
+function show(src, rect) {
     if (!wrapper) {
-        wrapper = document.createElement("div")
+        wrapper = document.createElement("img")
         wrapper.id = "eypi"
-    
-        wrapperImg = document.createElement("img")
-        wrapper.appendChild(wrapperImg)
+        wrapper.addEventListener("load", handleLoad)
     
         const s = document.createElement("style")
-        s.textContent = `
-        #eypi {display: grid; position: fixed; left: 0px; top: 0px; width: 100vw; height: 100vh; align-items: center; justify-items: center; pointer-events: none;} 
-        #eypi img {max-height: 80vh;}`
+        s.textContent = `#eypi {position: fixed; left: 0px; top: 0px; width: 0px; height: 0px; pointer-events: none; z-index: 999999999;}`
         document.head.appendChild(s)
+
+        window.addEventListener("scroll", this.handleScroll, {passive: true, capture: true})
     }
     wrapper.remove()
-    wrapperImg.src = src.split("=")[0] 
+    activeRect = rect 
+    wrapper.src = src.split("=")[0] 
+}
+
+function handleLoad(e) {
+    if (!activeRect || !(wrapper?.naturalWidth)) return 
+
+    let x = activeRect.x + activeRect.width + 20
+    let y = activeRect.y + 20
+    let width = wrapper.naturalWidth
+    let height = wrapper.naturalHeight
+    let ratio = width / height 
+    
+    
+    if (height > (window.innerHeight - 80)) {
+        y = 40 
+        height = window.innerHeight - 80
+        width = height * ratio 
+    }
+
+    
+    const maxWidth = (window.innerWidth - x - 40) 
+    if (width > maxWidth) {
+        width = maxWidth
+        height = width / ratio 
+    }
+
+
+    const floorX = x
+    const floorY = 40 
+
+    const ceilX = window.innerWidth - width - 40
+    const ceilY = window.innerHeight - height - 40 
+
+    x = Math.min(Math.max(floorX, x), ceilX)
+    y = Math.min(Math.max(floorY, y), ceilY)
+
+    wrapper.style.left = `${x}px`
+    wrapper.style.top = `${y}px`
+    wrapper.style.width = `${width}px`
+    wrapper.style.height = `${height}px`
     document.body.appendChild(wrapper)
-    isActive = true 
+}
+
+function handleScroll(e) {
+    activeRect && remove() 
 }
 
 function remove() {
     wrapper.remove()
-    wrapperImg.src = ""
-    isActive = false 
-}
+    wrapper.src = ""
+    activeRect = null 
+}    
 
 
 function handleMouseOver(e) {
-    isActive && remove() 
+    activeRect && remove() 
 
     const img = e.target
     if (!(img?.tagName === "IMG" && img.src.includes("https://yt3.ggpht.com"))) return 
@@ -58,12 +98,12 @@ function handleMouseOver(e) {
     const parent = img.parentElement
     if (!(parent?.tagName === "YT-IMG-SHADOW" && (parent.classList.contains("ytd-comment-renderer") || parent.classList.contains("ytd-video-owner-renderer")))) return 
 
-    e.stopImmediatePropagation()
-    show(img.src.split("=")[0])
+    e.stopImmediatePropagation();
+    show(img.src.split("=")[0], img.getBoundingClientRect())
 }
 
 function handleMouseOut(e) {
-    isActive && remove() 
+    activeRect && remove() 
     e.stopImmediatePropagation()
 }
 
